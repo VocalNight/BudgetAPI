dd8350ce446687372afb921e94ccd5f70222141f
Vocal <vocalnight@protonmail.com>
2024-02-03 23:09:12 -0300

    Changed the model

--- BudgetAPI/Models/TransactionModel.cs
+++ BudgetAPI/Models/TransactionModel.cs
@@ -4,6 +4,7 @@
     {
         public long Id {  get; set; }
         public string Name { get; set; }
+        public decimal Price { get; set; }
         public DateTime Date { get; set; }
         public CategoryModel Category { get; set; }
     }
