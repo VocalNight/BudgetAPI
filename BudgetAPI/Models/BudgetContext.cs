fa87d28774ff3e274ae03b55e517afb3d963f41b
Vocal <vocalnight@protonmail.com>
2024-02-05 18:22:27 -0300

    Added relationship in EF framework

--- BudgetAPI/Models/BudgetContext.cs
+++ BudgetAPI/Models/BudgetContext.cs
@@ -1,10 +1,20 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+using System.Reflection.Emit;
 
 namespace BudgetAPI.Models
 {
     public class BudgetContext : DbContext
     {
         public BudgetContext(DbContextOptions<BudgetContext> options) : base(options) { }
+
+        protected override void OnModelCreating( ModelBuilder modelBuilder )
+        {
+            modelBuilder.Entity<CategoryModel>()
+                .HasMany(c => c.Transactions)
+                .WithOne(e => e.Category)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
 
         public DbSet<CategoryModel> Categories { get; set; }
         public DbSet<TransactionModel> Transactions { get; set; }
--- BudgetAPI/Models/TransactionModel.cs
+++ BudgetAPI/Models/TransactionModel.cs
@@ -6,6 +6,8 @@
         public string Name { get; set; }
         public decimal Price { get; set; }
         public DateTime Date { get; set; }
+
+        public long? CategoryId { get; set; }
         public CategoryModel Category { get; set; }
     }
 }
